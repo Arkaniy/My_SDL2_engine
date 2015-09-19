89a03137c75b338a112a66ca4862e256e05bdcf9
Arkaniy <lexaalek333@gmail.com>
2015-09-19 11:49:10 +0300

    add enums

---
+++ enums.h
@@ -0,0 +1,7 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+enum ScreenState { SS_Game, SS_Menu, SS_Credits, SS_Quit, SS_TOTAL };
+enum WidgetEvent { WE_NewGame, WE_Menu, WE_Credits, WE_Quit, WE_TOTAL };
+
+#endif // ENUMS_H
