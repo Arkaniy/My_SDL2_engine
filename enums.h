38b7bd499fb17b3dae1804ff4b5a43928ae66e9d
Arkaniy <lexaalek333@gmail.com>
2015-10-02 18:57:37 +0300

    change enums structure

--- enums.h
+++ enums.h
@@ -2,6 +2,19 @@
 #define ENUMS_H
 
 enum ScreenState { SS_Game, SS_Menu, SS_Credits, SS_Quit, SS_TOTAL };
-enum WidgetEvent { WE_NewGame, WE_Menu, WE_Credits, WE_Quit, WE_TOTAL };
+
+enum BaseEvent { BE_NewGame, BE_Menu, BE_Credits, BE_Quit, BE_TOTAL };
+enum GameEvent { GE_TOTAL };
+struct WidgetEvent {
+	WidgetEvent() {}
+	WidgetEvent(BaseEvent baseEvent) { _baseEvent = baseEvent; }
+	WidgetEvent(GameEvent gameEvent) { _gameEvent = gameEvent; }
+
+	BaseEvent _baseEvent = BE_TOTAL;
+	GameEvent _gameEvent = GE_TOTAL;
+};
+
+enum TileType { TT_Passable, TT_Wall, TT_TOTAL};
+enum TilePicture { TP_BackgroundMenu, TP_BackgroundWait, TP_Button, TP_ButtonPressed, TP_Tile, TP_Man, TP_Man1, TP_Wall, TP_TOTAL };
 
 #endif // ENUMS_H
